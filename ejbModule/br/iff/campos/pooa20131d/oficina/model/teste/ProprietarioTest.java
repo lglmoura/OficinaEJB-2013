52f32bc9935dd2b5e1ef3086ef70681920239d28
Luiz Gustavo <lglmoura@gmail.com>
2013-06-20 20:19:07 -0300

    REsto dos Teste

--- ejbModule/br/iff/campos/pooa20131d/oficina/model/teste/ProprietarioTest.java
+++ ejbModule/br/iff/campos/pooa20131d/oficina/model/teste/ProprietarioTest.java
@@ -1,8 +1,8 @@
 package br.iff.campos.pooa20131d.oficina.model.teste;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.Test;
@@ -13,33 +13,44 @@ import br.iff.campos.pooa20131d.oficina.model.entity.Proprietario;
 public class ProprietarioTest {
 
 	private ProprietarioPersistence pprop = new ProprietarioPersistence();
-	private String uid = UUID.randomUUID().toString();
+	private String uid = "001";
 
 	@Test
 	public void testInserir() {
 		Proprietario prop = pprop.inserir(uid, "bairro", "cidade",
 				"complemento", "cpf", 20, "nome", "rua", "uf");
-		assertEquals(prop.getUid(),uid);
+		assertEquals(prop.getUid(), uid);
 	}
 
 	@Test
-	public void testUpdateStringStringStringStringStringIntegerStringStringString() {
-		fail("Not yet implemented");
+	public void testUpdate() {
+		Proprietario prop = pprop.update(uid, "bairroUPDATE", "cidade",
+				"complemento", "cpf", 20, "nome", "rua", "uf");
+		assertEquals(prop.getBairro(), "bairroUPDATE");
 	}
 
 	@Test
-	public void testFindString() {
-		fail("Not yet implemented");
-	}
+	public void testFind() {
+		Proprietario prop = pprop.find(uid);
+		assertEquals(prop.getCidade(), "cidade");
 
-	@Test
-	public void testDeleteString() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFindAll() {
-		fail("Not yet implemented");
+		List<Proprietario> lprpop = pprop.findAll();
+		for (Proprietario prop : lprpop) {
+			assertEquals(prop.getCidade(), "cidade");
+
+		}
+
+	}
+
+	@Test
+	public void testDelete() {
+		pprop.delete(uid);
+		
+
 	}
 
 }
